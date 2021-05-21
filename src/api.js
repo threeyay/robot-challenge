49458f9cb7beaf244de947ab68219cc265bd9b52
Abhimanyu Asokan <abhi@beigetech.com.au>
2021-05-21 10:16:32 +1000

    test code

--- src/api.js
+++ src/api.js
@@ -2,13 +2,61 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const _ = require('lodash');
 
-
+const Drone = require('./models/Drone');
+const Billboard = require('./models/Billboard');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 
+//starting now. 
 app.get('/', (req, res) => {
     res.json({foo: 'bar'});
 });
+function isValidInput(count, instruction) {
+    let regex = /^[wasdx]*$/;
+    if(count == undefined || instruction == undefined){
+        return false;
+    }
+    if (!instruction.match(regex)){
+        return false
+    }
+    if(Number.isNaN(count)){
+        return false;
+    }
+    if((!Number.isInteger(Number(count)))||(count<0)){
+        return false
+    }
+    return true;
+};
+//process drone instruction requests
+app.post('/drone', function(req, res) {
+    console.log(req.body);
+    var count;
+    var instructions;
+
+    count = req.body.droneCount;
+    instructions = req.body.droneInstructions;
+    if(!isValidInput(count,instructions)){
+        res.statusCode = 400;
+        res.send("invalid input");
+    }else{    
+        console.log('instructoins: ',instructions)
+        drones = [];
+        billboards = [];
+        for (let i = 0; i < count; i++) {
+            drones.push(new Drone(instructions.match(new RegExp('.{1,'+count+'}','g')).map(x=>x[i]).filter(x=>x!=null)));
+        }
+        drones.forEach(drone=>{
+            billboards.push(...drone.getBillboards());
+        });
+        console.log(billboards);
+        res.json({no_of_billboards: _.uniqWith(billboards,_.isEqual).length});
+    }
+});
+
 
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
 
--- src/client.js
+++ src/client.js
@@ -1,15 +1,101 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import useFetch from 'react-fetch-hook';
-
-
-function App() {
-    const {isLoading, data} = useFetch('http://localhost:4001');
-    if(isLoading) {
-        return 'Loading...';
+const axios = require('axios');
+class DroneInput extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            droneInstructions: '',
+            droneCount:'',
+            numberOfBillBoards:undefined,
+            message:'',
+            isValidInput:false
+        }
     }
-    return <pre>{JSON.stringify(data, null, 4)}</pre>;
+    inputCheck(){
+        let regex = /^[wasdx]*$/;
+        if(event.target.value <= 0){
+            this.setState({
+                isValidInput: false,
+                message: "count should be greater than 0"
+            })
+
+        }
+        else if(!event.target.value.match(regex)){
+            this.setState({
+                isValidInput: false,
+                message:"Instruction should contain only the following characters: x, ^, v, <, >"
+            });
+        }else{
+            this.setState({
+                isValidInput: true,
+                message:""
+            })
+        }
+    }
+    handleCountChange = event => {
+        this.inputCheck();
+		this.setState({
+			droneCount: event.target.value
+        })
+        
+	}
+
+	handleInstructionsChange = event => {
+        this.inputCheck();
+		this.setState({
+			droneInstructions: event.target.value
+		})
+	}
+    URL = "http://localhost:4001/drone";
+    handleSubmit = (e) => {
+        this.setState({
+            numberOfBillBoards:undefined
+        })
+        e.preventDefault();
+        console.log(this.state);
+        var self = this;
+        axios.post("http://localhost:4001/drone", {
+            droneCount:this.state.droneCount,
+            droneInstructions:this.state.droneInstructions
+        })
+          .then(function (response) {
+            self.setState({
+                numberOfBillBoards: response.data.no_of_billboards
+            });
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    };
+    render(){
+        const {droneInstructions, droneCount} = this.state;
+        return(
+        <div id="droneInput">
+          <h1>Drone Input</h1>
+            <form onSubmit={this.handleSubmit}>
+                <label>No. Of Drones: </label>
+                <input type="number" name = "count" value = {droneCount} onChange={this.handleCountChange}/>
+                <br/><br/>
+                <label>Instructions: </label>
+                <input type="text" name = "instructions" value = {droneInstructions} onChange={this.handleInstructionsChange}/>
+                <br/><br/>
+                <button type="submit" disabled={!this.state.isValidInput}>Submit</button>
+            </form>
+           <h3>Number of Billboards photographed: {this.state.numberOfBillBoards}</h3>
+           <p>{this.state.message}</p>
+        </div>
+        );
+    }
 }
+// function App() {
+//     const {isLoading, data} = useFetch('http://localhost:4001');
+//     if(isLoading) {
+//         return 'Loading...';
+//     }
+//     return <pre>{JSON.stringify(data, null, 4)}</pre>;
+// }
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<DroneInput />, document.getElementById('app'));
